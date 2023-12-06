f2a7f124e72218f6136f940bff817dd2ab1b0244
hugo-ravailhe <hugo.rh@free.fr>
2023-12-06 12:01:27 +0100

    Refactor in class Train and Reservation

--- python/ticket_office/app.py
+++ python/ticket_office/app.py
@@ -1,36 +1,55 @@
 import json
 import requests
 from flask import Flask, request
-from refractorFunc import check_available_seats, reserve_seats, create_reservation
+
+class Train:
+    def __init__(self, train_id):
+        self.id = train_id
+        self.data = requests.get(f"http://localhost:8081/data_for_train/{self.id}").json()
+
+    def check_available_seats(self):
+        return filter(lambda s: s["coach"] == "A" and not s["booking_reference"], self.data["seats"].values())
+    
+class Reservation:
+    def __init__(self, train: Train, booking_reference, to_reserve):
+        self.train = train
+        self.booking_reference = booking_reference
+        self.seat_ids = [s["seat_number"] + s["coach"] for s in to_reserve]
+    
+    def get_payload(self):
+        return {
+            "train_id": self.train.id,
+            "seats": self.seat_ids,
+            "booking_reference": self.booking_reference
+        }
+    
+def reserve_seats(available_seats, seat_count):
+    return [next(available_seats) for _ in range(seat_count)]
 
 def create_app():
     app = Flask("ticket_office")
 
-    def send_reservation(reservation):
-        reservation_payload = {
-            "train_id": reservation["train_id"],
-            "seats": reservation["seats"],
-            "booking_reference": reservation["booking_reference"]
-        }
+    def send_reservation(reservation: Reservation):
+        reservation_payload = reservation.get_payload()
         response = requests.post("http://localhost:8081/reserve", json=reservation_payload)
         return response
 
     @app.post("/reserve")
     def reserve():
         payload = request.json
-        seat_count, train_id = payload["count"], payload["train_id"]
+        train = Train(payload["train_id"])
+        seat_count = payload["count"]
         
         booking_reference = requests.get("http://localhost:8082/booking_reference").text
-        train_data = requests.get(f"http://localhost:8081/data_for_train/{train_id}").json()
 
-        available_seats = check_available_seats(train_data)
+        available_seats = train.check_available_seats()
         to_reserve = reserve_seats(available_seats, seat_count)
-        reservation = create_reservation(to_reserve, train_id, booking_reference)
+        reservation = Reservation(train, booking_reference, to_reserve)
 
         response = send_reservation(reservation)
         assert response.status_code == 200, response.text
 
-        return json.dumps(reservation)
+        return json.dumps(reservation.get_payload())
 
     return app
 
